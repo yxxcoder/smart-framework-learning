4db8763277cdcd94738bb8d5673a37175b4eec44
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-07-08 23:57:24 +0800

    BeanHelper

---
+++ smart-framework-ioc/smart-ioc/src/main/java/com/simple/smart/ioc/annotation/Inject.java
@@ -0,0 +1,16 @@
+package com.simple.smart.ioc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 依赖注入注解
+ *
+ * @author yxxcoder
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
